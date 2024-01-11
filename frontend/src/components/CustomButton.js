666e9294d17936f34d6d70b6daf4ef53c7c15893
yabukauzki <yabuuchi@ss.cs.osakafu-u.ac.jp>
2024-01-11 19:00:10 +0900

    Fix: フロントの調整useParamとaxiosを追加

--- frontend/src/components/CustomButton.js
+++ frontend/src/components/CustomButton.js
@@ -13,12 +13,8 @@ const styleButton = {
 // ボタンのテキストと遷移先のページをpropsとして受け取る
 const CustomButton = (props) => {
   // window.locationを使う
-  const query = new URLSearchParams();
   const handleClick = () => {
-    query.append("station", props.text);
-    // "/osaka"にリダイレクトする
-    window.location.href = "/osaka?" + query.toString();
-    window.location.href = "./Result?" + query.toString();
+    window.location.href = "/Result/" + props.text;
   };
 
   // Buttonコンポーネントを返す
